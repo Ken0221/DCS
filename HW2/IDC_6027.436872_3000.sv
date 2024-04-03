3e11ab34b5fc2902489e94a4933425876be58d5d
Ken <85241458+Ken0221@users.noreply.github.com>
2024-04-03 16:00:24 +0800

    Add files via upload

---
+++ HW2/IDC_6027.436872_3000.sv
@@ -0,0 +1,64 @@
+module IDC(
+    // Input signals
+	clk,
+	rst_n,
+	in_valid,
+    in_id,
+    // Output signals
+    out_valid,
+    out_legal_id
+);
+
+//---------------------------------------------------------------------
+//   INPUT AND OUTPUT DECLARATION                         
+//---------------------------------------------------------------------
+input clk, rst_n, in_valid;
+input [5:0] in_id;
+
+output logic out_valid;
+output logic out_legal_id;
+
+//---------------------------------------------------------------------
+//   LOGIC DECLARATION
+//---------------------------------------------------------------------
+logic [3:0] n0, n1;
+logic [8:0] sum;
+logic [3:0] w1;
+//---------------------------------------------------------------------
+//   Your design                        
+//---------------------------------------------------------------------
+always @(posedge clk, negedge rst_n) begin
+    if(!rst_n) begin
+        out_valid <= 0;
+        out_legal_id <= 0;
+        sum <= 0;
+        w1 <= 10;
+        n0 <= 0;
+        n1 <= 0;
+    end
+    else begin
+        if(in_valid) begin
+            n0 <= in_id / 10;
+            n1 <= in_id % 10;
+            sum <= sum + (n1 * w1) + n0;
+            w1 <= w1 - 1;
+        end
+        if(w1 == 0) begin
+            if(n1 == (10 - (sum % 10)) % 10) begin
+                out_legal_id <= 1;
+            end
+            else begin
+                out_legal_id <= 0;
+            end
+            out_valid <= 1;
+        end
+        if(out_valid == 1) begin
+            out_valid <= 0;
+            out_legal_id <= 0;
+            sum <= 0;
+            w1 <= 10;
+        end
+    end
+end
+
+endmodule
