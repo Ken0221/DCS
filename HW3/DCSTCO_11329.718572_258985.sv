b0516171e8a34bbe2a62b4fc34e0b2973e29397f
Ken0221 <85241458+Ken0221@users.noreply.github.com>
2024-04-17 14:07:46 +0800

    upload testing

--- HW3/DCSTCO_11329.718572_258985.sv
+++ HW3/DCSTCO_11329.718572_258985.sv
@@ -19,6 +19,7 @@ module DCSTCO(
 	product_out,
 	number_out
 );
+//testing
 
 //---------------------------------------------------------------------
 //   INPUT AND OUTPUT DECLARATION                         
